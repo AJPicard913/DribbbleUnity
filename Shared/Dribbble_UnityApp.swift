4753460abf9ee570586c206f437c2f855929a0d3
ajpicard913 <Contact@saucedesigns.io>
2020-09-25 14:43:49 -0400

    Initial Commit

---
+++ Shared/Dribbble_UnityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dribbble_UnityApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct Dribbble_UnityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
